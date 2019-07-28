5610f739bd48b7488fb1919f9ffcbeec336cf9e2
Muream <muream@gmail.com>
2019-07-28 23:58:21 +0200

    experiment with various stylesheets

--- asset_manager/__main__.py
+++ asset_manager/__main__.py
@@ -1,5 +1,9 @@
 import sys
 
+import qdarkstyle
+import qtmodern.styles
+import qtmodern.windows
+
 from PySide2.QtWidgets import QApplication
 from asset_manager.ui.window import AssetManagerWindow
 
@@ -8,7 +12,22 @@ def main():
     app = QApplication(sys.argv)
 
     window = AssetManagerWindow()
+
+    # use qdarkstyle stylesheet
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyside2())
+
+    # Use QTDark theme
+    # stylesheet = "asset_manager/QTDark.stylesheet"
+    # with open(stylesheet, "r") as f:
+    #     app.setStyleSheet(f.read())
+
     window.show()
+
+    # Use Qtmodern stylesheet
+    # qtmodern.styles.dark(app)
+    # mw = qtmodern.windows.ModernWindow(window)
+    # mw.show()
+
     sys.exit(app.exec_())
 
 
