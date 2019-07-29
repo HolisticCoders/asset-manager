270faddca77007c125ecbb536dc6d5f7675e1201
Muream <muream@gmail.com>
2019-07-29 20:56:13 +0200

    WIP: user settings implementation

--- asset_manager/api/config/__init__.py
+++ asset_manager/api/config/__init__.py
@@ -1,4 +1,7 @@
+import appdirs
+import json
 import logging
+import os
 
 
 logger = logging.getLogger(__name__)
@@ -7,6 +10,28 @@ logger = logging.getLogger(__name__)
 from .default_config import *
 
 try:
-    from asset_manager_user_config import *
+    from asset_manager_config import *
 except ImportError:
-    logger.info("No user config found"
+    logger.info("No user config found")
+
+
+def _settings_path():
+    config_folder = appdirs.user_config_dir("asset-manager")
+    return os.path.join(config_folder, "config.json")
+
+
+def user_settings() -> dict:
+    settings_path = _settings_path()
+
+    if not os.path.exists(settings_path):
+        return {}
+    else:
+        with open(settings_path, "r") as f:
+            return json.loads(f.read())
+
+
+def set_user_settings(settings: dict) -> None:
+    settings_path = _settings_path()
+    with open(settings_path, "w") as f:
+        f.write(json.dumps(settings))
+
