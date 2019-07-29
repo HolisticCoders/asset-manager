adf01d4c97819ffa517a2b0b79133f5ede3e4b79
Muream <muream@gmail.com>
2019-07-29 11:34:53 +0200

    remove unused themes

--- asset_manager/__main__.py
+++ asset_manager/__main__.py
@@ -1,8 +1,6 @@
 import sys
 
 import qdarkstyle
-import qtmodern.styles
-import qtmodern.windows
 
 from PySide2.QtWidgets import QApplication
 from asset_manager.ui.window import AssetManagerWindow
@@ -12,22 +10,8 @@ def main():
     app = QApplication(sys.argv)
 
     window = AssetManagerWindow()
-
-    # use qdarkstyle stylesheet
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyside2())
-
-    # Use QTDark theme
-    # stylesheet = "asset_manager/QTDark.stylesheet"
-    # with open(stylesheet, "r") as f:
-    #     app.setStyleSheet(f.read())
-
     window.show()
-
-    # Use Qtmodern stylesheet
-    # qtmodern.styles.dark(app)
-    # mw = qtmodern.windows.ModernWindow(window)
-    # mw.show()
-
     sys.exit(app.exec_())
 
 
