911a2b8397f18930d511064855ce8d1bf5150239
Loïc Pinsard <muream@gmail.com>
2019-08-06 21:39:12 +0200

    temporarly list all files instead of just folders

--- asset_manager/api/files.py
+++ asset_manager/api/files.py
@@ -4,7 +4,8 @@ from pydrive.files import GoogleDriveFileList
 
 def list_children(google_drive: GoogleDrive, parent_id: str) -> GoogleDriveFileList:
     metadata = {
-        "q": f"'{parent_id}' in parents and trashed=false and mimeType='application/vnd.google-apps.folder'",
+        # "q": f"'{parent_id}' in parents and trashed=false and mimeType='application/vnd.google-apps.folder'",
+        "q": f"'{parent_id}' in parents and trashed=false",
         "orderBy": "title",
     }
     return google_drive.ListFile(metadata).GetList()
