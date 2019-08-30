b1edc14215812e66d02a4cfd7835829853babb67
Muream <muream@gmail.com>
2019-08-30 16:12:19 +0200

    remove tests

--- tests/api/test_asset.py
+++
@@ -1,9 +0,0 @@
-from asset_manager.api.asset import AssetModel
-from asset_manager.api.config import FOLDER_IDS
-
-
-class TestAssetModel:
-    def test_qt_model(self, google_drive, qtmodeltester):
-        model = AssetModel(google_drive, FOLDER_IDS)
-        qtmodeltester.check(model)
-
